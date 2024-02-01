1bed836339ea4b861f60b77c70bab6dc4a3a2340
utaek_desk <utaek777@gmail.com>
2024-02-02 07:03:42 +0900

    .

---
+++ src/main/java/com/uts3back/UtS3BackApplication.java
@@ -0,0 +1,13 @@
+package com.uts3back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtS3BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtS3BackApplication.class, args);
+    }
+
+}
