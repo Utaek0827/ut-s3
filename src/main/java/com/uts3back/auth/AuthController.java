e5f9c131ca4973870174893733e1093443fe1201
utaek_mac <utaek777@gmail.com>
2024-02-29 16:45:39 +0900

    front 연동

--- src/main/java/com/uts3back/auth/AuthController.java
+++ src/main/java/com/uts3back/auth/AuthController.java
@@ -1,18 +1,16 @@
 package com.uts3back.auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    @GetMapping("{sign-in}")
-    public String signIn(){
-        System.out.println("api test");
-        return "hello world";
+    @PostMapping("/SignIn")
+    public String signIn(
+            ){
+        System.out.println("ㅁㄴㅇㄹㅁ");
+        return "email+password";
     }
-
-
 }
+
